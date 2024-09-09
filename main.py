15219de3d75573c6d40c8372f50ee1ebf5eed710
Okke Warner <okke.warner@gmail.com>
2024-09-09 22:34:03 +0200

    changed variable names and input parameters

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from scipy.optimize import minimize
 import numpy as np
 
 # Einschränkungen: nur symmetrische Laminate mit gerader Anzahl von schichten TODO: Check if statement is true
+# TODO: Knockdown Factor
+# TODO: Constraints - 10% plyshare, strength?, only integer values for angles
+########################## PARAMETERS ##########################
 a = 400  # 400mm-800mm
 b = 250  # 150mm-250mm
 alpha = a / b
@@ -9,13 +12,18 @@ N_x = -1000  # -500N to -2000N
 N_y = -500  # 300N to -1000N
 Tau = 100  # 100 to 800N
 
-beta = N_y / N_x
 tLayer = 0.184  # 0.184mm to 0.25mm
 
 E11 = 130_000  # 130_000 to 160_000 MPa
 E22 = 10_000  # 10_000 to 14_000 MPa
 G12 = 5_000  # 5_000 to 8_000 MPa
 
+minLayers = 40
+maxLayers = 100
+maxDecimals = 0 #0 => Angles of result are only integer values
+
+########################## CALCULATIONS ##########################
+beta = N_y / N_x
 nu12 = 0.33  # TODO: Needs to be calculated????
 nu21 = E22 / E11 * nu12
 
@@ -156,6 +164,7 @@ def tau_cr(b, t, D11, D12, D22, D66):
 
 
 # helper function to find amount of halfwaves for minimal stability
+# for finding the halfwaves to make the plates as unstable as possible it is only necessary to use sig_biax as tau_cr is idemepent of the max number of halfwaves
 def halfwavesObj(hWaves, b, t, beta, alpha, D11, D12, D22, D66):
     m, n = hWaves
     return sig_x_cr_biax(b, t, beta, alpha, round(m), round(n), D11, D12, D22, D66)
@@ -164,57 +173,64 @@ def halfwavesObj(hWaves, b, t, beta, alpha, D11, D12, D22, D66):
 def R_panelbuckling_comb(sym_angles):
     m_numLayers = len(sym_angles) * 2
     m_hPanel = m_numLayers * tLayer
-    matD = D_mat(sym_angles)
+    m_matD = D_mat(sym_angles)
 
-    D11 = matD[0][0]
-    D12 = matD[0][1]
-    D22 = matD[1][1]
-    D66 = matD[2][2]
+    m_D11 = m_matD[0][0]
+    m_D12 = m_matD[0][1]
+    m_D22 = m_matD[1][1]
+    m_D66 = m_matD[2][2]
 
-    initialHWaves = np.ones(2)
-    hWaveBnd = (1, 5)
-    hWavesBnds = [hWaveBnd, hWaveBnd]
+    m_initialHWaves = np.ones(2)  # [1,1]
+    m_hWaveBnd = (1, 5)
+    m_hWavesBnds = [m_hWaveBnd, m_hWaveBnd]
 
     # Find amount of halfwaves that make plate as unstable as possible
-    result = minimize(halfwavesObj, initialHWaves, method='SLSQP', bounds=hWavesBnds,
-                      args=(b, m_hPanel, beta, alpha, D11, D12, D22, D66))
+    m_mostUnstablePlate = minimize(halfwavesObj, m_initialHWaves, method='SLSQP', bounds=m_hWavesBnds,
+                                   args=(b, m_hPanel, beta, alpha, m_D11, m_D12, m_D22, m_D66))
 
     # critical loads
-    m_sigma_x_cr = result.fun
-    m_tau_cr = tau_cr(b, m_hPanel, D11, D12, D22, D66)
+    m_sigma_x_cr = m_mostUnstablePlate.fun
+    m_tau_cr = tau_cr(b, m_hPanel, m_D11, m_D12, m_D22, m_D66)
 
     # applied loads
     m_sigma_x = N_x / m_hPanel
     m_tau = Tau / m_hPanel
 
     # R values
-    R_biax = abs(m_sigma_x / m_sigma_x_cr)
-    R_shear = abs(m_tau / m_tau_cr)
+    m_R_biax = abs(m_sigma_x / m_sigma_x_cr)
+    m_R_shear = abs(m_tau / m_tau_cr)
 
-    R = R_biax + R_shear ** 2
+    m_R = m_R_biax + m_R_shear ** 2
 
     # Debug - remove - hurts performance
-    print(1 / R, "-", m_numLayers)
-    return R
+    print("RF:", 1 / m_R, "- numLayers:", m_numLayers, "- numHalfwaves: ", m_mostUnstablePlate.x, "- Angles:", sym_angles)
+    return m_R
 
 
 def optimizeAngles(numLayers, initialSymAngles, iterCount):
     # Define bounds for angles
-    angleBounds = [(-90, 90) for _ in range(numLayers)]
+    m_angleBounds = [(-90, 90) for _ in range(numLayers)]
 
-    con1 = {'type': 'eq', 'fun': lambda sym_angles: A_mat(sym_angles)[0][2]}  # balanced
-    con2 = {'type': 'eq', 'fun': lambda sym_angles: A_mat(sym_angles)[1][2]}  # balanced
-    con3 = {'type': 'ineq', 'fun': lambda sym_angles: 1 - R_panelbuckling_comb(sym_angles)}  # panel does not buckle
-    con4 = "strength check"  # TODO: Necessary?
-    con5 = "10% of each ply share"
-    cons = [con1, con2, con3]
+    # BALANCED PLY
+    m_con1 = {'type': 'eq', 'fun': lambda sym_angles: A_mat(sym_angles)[0][2]}
+    m_con2 = {'type': 'eq', 'fun': lambda sym_angles: A_mat(sym_angles)[1][2]}
 
-    options = {'maxiter': iterCount}
+    # PLY DOES NOT BUCKLE
+    m_con3 = {'type': 'ineq', 'fun': lambda sym_angles: 1 - R_panelbuckling_comb(sym_angles)}
 
-    solution = minimize(R_panelbuckling_comb, initialSymAngles, method='trust-constr', bounds=angleBounds,
-                        constraints=cons,
-                        options=options)
-    return solution
+    # ANGLES ONLY HAVE SOME AMOUNT OF DECIMALS TODO: FIX NEEDED
+    m_con4 = {'type': 'eq', 'fun': lambda sym_angles: np.amax(abs(np.around(sym_angles, decimals=maxDecimals) - sym_angles))}
+
+    # m_con5 = "strength check"  # TODO: Necessary?
+    # m_con6 = "10% of each ply share"
+    m_cons = [m_con1, m_con2, m_con3]
+
+    m_options = {'maxiter': iterCount}
+
+    m_optimal_R = minimize(R_panelbuckling_comb, initialSymAngles, method='trust-constr', bounds=m_angleBounds,
+                           constraints=m_cons,
+                           options=m_options)
+    return m_optimal_R
 
 
 def T_mat(theta):
@@ -227,22 +243,8 @@ def T_mat(theta):
     ])
 
 
-def RF_strength(sym_angles):
-    numLayers = len(sym_angles) * 2
-    h = numLayers * tLayer
-    # Inverse of ABD
-    # Multiply applied Loads with ABD^-1 => epsylon_0 and k
-    # calculate epsylon_x, epsylon_y, gamma_xy for each ply
-    # transform all epsylon into material cos
-    # use Q matrix to calculate each sigma_1, sigma_2, sigma_12
-    # calculate RF_ff and RF_iff for each layer
-    # return appropriate value for failure criterion
-
-
-minSymLayers = 22
-maxSymLayers = 22
-optimalSymLayers = maxSymLayers
-optimalSymAngles = []
+minSymLayers = int(minLayers / 2)
+maxSymLayers = int(maxLayers / 2)
 
 
 def optimalLayers(initAngle):
