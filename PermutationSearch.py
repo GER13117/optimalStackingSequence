f4da4e132bd27d6cc32a8f3c6c4a6083e09d084d
Okke Warner <okke.warner@gmail.com>
2024-09-11 18:40:16 +0200

    output changes

--- PermutationSearch.py
+++ PermutationSearch.py
@@ -2,9 +2,25 @@ import MaterialFunc as mf
 import numpy as np
 from scipy.optimize import minimize
 import random
-#explained by https://www.sciencedirect.com/science/article/pii/S0263822314005492
+
+
+# explained by https://www.sciencedirect.com/science/article/pii/S0263822314005492
 
 # possibleAngles = [90, 0, 45, -45]
+
+def progress_bar(counter, max_value, bar_length=40, additional_value=None):
+    # Calculate progress as a percentage
+    progress = counter / max_value
+    # Calculate the number of hashes (#) in the bar
+    block = int(round(bar_length * progress))
+    # Create the progress bar string
+    progress_bar = "#" * block + "-" * (bar_length - block)
+
+    # Display the progress bar with the percentage
+    if additional_value is not None:
+        print(f"\r[{progress_bar}] {progress * 100:.2f}% - {additional_value}", end="")
+    else:
+        print(f"\r[{progress_bar}] {progress * 100:.2f}%", end="")
 
 def geneatePossibleAngles(start: float, end: float, interval: float):
     numbers = []
@@ -15,13 +31,23 @@ def geneatePossibleAngles(start: float, end: float, interval: float):
     return numbers
 
 
+def checkPlyShare(stackingSeq: list):
+    dedupSeq = list(dict.fromkeys(stackingSeq))
+    shareArray = []
+    for angle in dedupSeq:
+        plyShare = stackingSeq.count(angle) / len(stackingSeq)
+        shareArray.append(plyShare)
+    return max(shareArray) <= 0.1
+
+
 def generateRandomStack(m_availableAngles, m_numSymAngles):
     stackingSeq = [random.choice(m_availableAngles) for _ in range(m_numSymAngles)]
     stackingSeq = [90.0 if angle == -90.0 else angle for angle in stackingSeq]
     return stackingSeq
 
+
 def generateRandomStackPairs(m_availableAngles, m_numSymAngles):
-    stackingSeq = [random.choice(m_availableAngles) for _ in range(int(m_numSymAngles/2))]
+    stackingSeq = [random.choice(m_availableAngles) for _ in range(int(m_numSymAngles / 2))]
     negAngles = [-x for x in stackingSeq]
     stackingSeq.extend(negAngles)
     if m_numSymAngles % 2 != 0:
@@ -38,16 +64,15 @@ def stackIsBalanced(stackingSeq):
 def generateStackingSeqs(numSymLayers: int, numStackingSeqs: int, interval: float):
     m_initialSequences = []
     print("Generating stacking sequences")
-    for i in range(numStackingSeqs):
-        print(".", end='')
-    print(" ")
     m_possibleAngles = geneatePossibleAngles(-90.0, 90.0, interval)
     while len(m_initialSequences) < numStackingSeqs:
-        #m_possibleStackingSeq = generateRandomStack(m_possibleAngles, numSymLayers)
+        # m_possibleStackingSeq = generateRandomStack(m_possibleAngles, numSymLayers)
         m_possibleStackingSeq = generateRandomStackPairs(m_possibleAngles, numSymLayers)
-        if stackIsBalanced(m_possibleStackingSeq) and not (m_possibleStackingSeq in m_initialSequences):
+        if stackIsBalanced(m_possibleStackingSeq) and not (
+                m_possibleStackingSeq in m_initialSequences) and checkPlyShare(m_possibleStackingSeq):
             m_initialSequences.append(m_possibleStackingSeq)
-            print(".", end='')
+        progress_bar(len(m_initialSequences), numStackingSeqs,
+                     additional_value=f"{len(m_initialSequences)}/{numStackingSeqs}")
     print(" ")
     return m_initialSequences
 
@@ -66,6 +91,12 @@ def R_panelbuckling_comb(stackingSeq: list, alpha, b, beta, tLayer, N_x, Tau):
     m_D66 = m_matD[2][2]
     m, n = 1, 1  # TODO: minimizer
     m_hPanel = tLayer * len(stackingSeq)
+    m_sigma_x_cr = mf.sig_x_cr_biax(b, m_hPanel, beta, alpha, m, n, m_D11, m_D12, m_D22, m_D66)
+
+    for i in range(1, 4):
+        for j in range(1, 4):
+            m_sigma_x_cr = min(m_sigma_x_cr, mf.sig_x_cr_biax(b, m_hPanel, beta, alpha, i, j, m_D11, m_D12, m_D22, m_D66))
+
     m_sigma_x_cr = mf.sig_x_cr_biax(b, m_hPanel, beta, alpha, m, n, m_D11, m_D12, m_D22, m_D66)
     m_tau_cr = mf.tau_cr(b, m_hPanel, m_D11, m_D12, m_D22, m_D66)
 
@@ -86,9 +117,11 @@ def optimizeLayers(numSymLayers: int, numStackingSeqs: int, alpha, b, beta, tLay
     stackingSeqsRev = generateStackingSeqs(numSymLayers, numStackingSeqs, interval)
     bestR = 10000
     bestStackingSeqRev = stackingSeqsRev[0]
-    for stackingSeqRev in stackingSeqsRev:
+    print("Optimizing Stacking Sequence:")
+    for idxSeq, stackingSeqRev in enumerate(stackingSeqsRev):
         bestCurrentR = R_panelbuckling_comb(stackingSeqRev, alpha, b, beta, tLayer, N_x, Tau)
         bestCurrentSeqRev = stackingSeqRev[:]
+        progress_bar(idxSeq + 1, len(stackingSeqsRev), additional_value=f"Best RF: {1/bestR:.3f}")
         for i in range(numSymLayers):
             for j in range(i + 1, numSymLayers):
                 swappedStackingSeqRev = swapValues(stackingSeqRev, i, j)
@@ -105,7 +138,10 @@ def optimizeLayers(numSymLayers: int, numStackingSeqs: int, alpha, b, beta, tLay
                 # print("i:", i, "stacking Sequence:", stackingSeqRev[::-1], "R:", R)
             stackingSeqRev = bestCurrentSeqRev[:]
             # print("i:", i, "best current Sequence:", stackingSeqRev[::-1], "R:", bestCurrentR)
-        print("best final Sequence:", stackingSeqRev[::-1], "R:", bestCurrentR, "is balanced:",
-              stackIsBalanced(stackingSeqRev[::-1]))
-
-    print("Best Stacking Sequence:", bestStackingSeqRev[::-1], "R:", bestR)
+        #print("R:", round(bestCurrentR, 3), "RF:", round(1 / bestCurrentR, 3))
+    print(" ")
+    bestStackingSeq = bestStackingSeqRev[::-1]
+    print("Best Stacking Sequence:", bestStackingSeq,
+          "\nR:", round(bestR, 3), "RF:", round(1 / bestR, 3),
+          "\nis balanced: ", stackIsBalanced(bestStackingSeq),
+          "has max 10% plyshare:", checkPlyShare(bestStackingSeq))
--- main.py
+++ main.py
@@ -12,7 +12,7 @@ tLayer = 0.184  # 0.184mm to 0.25mm
 
 def main():
     #GradientOptimize.optimalLayers(45, 44, 44, 2, N_x, Tau, beta, b, alpha)
-    PermutationSearch.optimizeLayers(22, 100, alpha, b, beta, tLayer, N_x, Tau, 10)
+    PermutationSearch.optimizeLayers(22, 1000, alpha, b, beta, tLayer, N_x, Tau, 5)
 
 
 if __name__ == "__main__":
